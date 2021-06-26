7116986212545697de59c91627134e2ba3e062a4
Jaiharishan <downdkar@gmail.com>
2021-06-26 19:49:28 +0530

    changes made

--- javascript/main.js
+++ javascript/main.js
@@ -3,6 +3,5 @@
 // SCROLL FUNCTION FOR NAVBAR TO RESIZE
 window.addEventListener('scroll', ()=> {
     let topBar = document.querySelector('.top-bar');
-    // topBar.style.padding = '.4em .8em';
     topBar.classList.toggle('sticky', window.scrollY > 0);
 })
