c143a0d1e54677f5563f0e513ca91f42a7fdb0ac
Jaiharishan <downdkar@gmail.com>
2021-06-26 22:36:49 +0530

    made changes

--- javascript/main.js
+++ javascript/main.js
@@ -5,3 +5,28 @@ window.addEventListener('scroll', ()=> {
     let topBar = document.querySelector('.top-bar');
     topBar.classList.toggle('sticky', window.scrollY > 0);
 })
+
+
+// SWITCH IMAGES IN CLICK
+
+// ARRAY TO STORE CONTENT
+let gameNews = {
+    
+}
+
+let mainGameImgContainer = document.querySelector('.game-img');
+let sideGameImgContainer = document.querySelector('.side-game-imgs');
+
+let sideGameImg = document.querySelectorAll('.side-game-img');
+
+
+for (let i = 0; i < sideGameImg.length; i++) {
+    sideGameImg[i].addEventListener('click', ()=> {
+        let childImg = sideGameImg[i].children[0];
+        let mainImg = mainGameImgContainer.querySelector('.main-img');
+        mainGameImgContainer.appendChild(childImg);
+        childImg.getElementsByClassName.order = '1';
+        childImg.setAttribute('class', 'main-img');
+        sideGameImg[i].appendChild(mainImg);
+    })
+}
