90aa2996ff323c16b2302c85adcae3b54fb95640
Jaiharishan <downdkar@gmail.com>
2021-06-27 10:57:24 +0530

    Added Changes

--- javascript/main.js
+++ javascript/main.js
@@ -11,13 +11,19 @@ window.addEventListener('scroll', ()=> {
 
 // ARRAY TO STORE CONTENT
 let gameNews = {
-    
+    'pubg':'Pubg State reaches 17M downloads on playStore',
+    'fortnite':"Fortnite Creative featured Guidlines | see what's new",
+    'ApexLegends':"Apex Legends Genesis Patch, What's new in patch 9.1",
+    'csgo':'The road to Stockholm 2021, gear up for more updates',
+    'dota2':'Nemistice falls and new Power rises, the GameChanger?',
 }
 
 let mainGameImgContainer = document.querySelector('.game-img');
 let sideGameImgContainer = document.querySelector('.side-game-imgs');
 
 let sideGameImg = document.querySelectorAll('.side-game-img');
+let gameNameContainer = document.querySelector('#title');
+let gameNewsContainer = document.querySelector('.game-content > .sub-header');
 
 
 for (let i = 0; i < sideGameImg.length; i++) {
@@ -25,8 +31,12 @@ for (let i = 0; i < sideGameImg.length; i++) {
         let childImg = sideGameImg[i].children[0];
         let mainImg = mainGameImgContainer.querySelector('.main-img');
         mainGameImgContainer.appendChild(childImg);
+
         childImg.getElementsByClassName.order = '1';
         childImg.setAttribute('class', 'main-img');
         sideGameImg[i].appendChild(mainImg);
+        gameNewsContainer.textContent = gameNews[childImg.id];
+        gameNameContainer.textContent = childImg.id;
     })
 }
+
